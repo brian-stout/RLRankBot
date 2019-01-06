3802fc3fbb0a1ed951f90b73db0bca08220b0362
Brian Stout <brianjonstout@gmail.com>
2019-01-06 16:55:04 -0500

    Changed .gitignore, changed auth.json.

--- bot.js
+++ bot.js
@@ -1,10 +1,15 @@
-/**
- * A ping pong bot, whenever you send "ping", it replies "pong".
- */
-
 // Import the discord.js module
 const Discord = require('discord.js');
 const auth = require('./auth.json');
+const request = require('request');
+
+request('https://rocketleague.tracker.network/profile/steam/tasogare', { json: true }, (err, res, body) => {
+
+    if (err) { return console.log(err); }
+
+    console.log(body.url);
+    console.log(body.explanation);
+});
 
 // Create an instance of a Discord client
 const client = new Discord.Client();
@@ -28,12 +33,11 @@ function processCommand(client, message) {
     args = args.splice(1);
 
     switch (cmd) {
-        case 'ping':
-            message.channel.send('pong');
+        case 'hooah': 
+            message.channel.send('Hoooaaaah!');
             console.log(args);
             break;
     }
 };
 
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(auth.token);
